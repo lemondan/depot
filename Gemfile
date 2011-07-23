2f1462d47d4ac8e3e3733f0c9cdcd372b466904f
unknown <Administrator@.(none)>
2011-07-23 21:39:29 +0800

    create project

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Asset template engines
+#gem 'coffee-script'
+
+#gem 'jquery-rails'
+
+
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
+#gem 'will_paginate','3.0.pre'
+
+#gem 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ede01a7acc15df7764289a1e5cde58666ba78d62133a885c20d6071cdddf66682758079ae93a761874547575ff23abd40d104829193ab4f2f61941efab5a0505'
